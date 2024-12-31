fcdd13896d90dadbbbdc2948514d9f7222aa3e97
Gyan Deep <173229601+githubgyan-creater@users.noreply.github.com>
2024-12-31 11:55:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() { 
+        var preloader = document.getElementById('preloader');
+        preloader.style.display = 'none'; 
+    }, 3000); // Delay hiding the preloader for 3 seconds
+};
